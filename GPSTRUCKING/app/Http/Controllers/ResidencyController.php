59e5df4774e77bdda082b78d68f9345b1f72bee2
Emman-pip <parrenoemmanuel756@gmail.com>
2025-11-14 07:19:34 +0000

    Make sure there is only one residency record.

--- GPSTRUCKING/app/Http/Controllers/ResidencyController.php
+++ GPSTRUCKING/app/Http/Controllers/ResidencyController.php
@@ -11,10 +11,12 @@ use Inertia\Inertia;
 class ResidencyController extends Controller
 {
     public function form(){
+        if (Auth::user()->residency) return redirect()->route('resident.dashboard');
         return Inertia::render('resident/profileForm', [ 'barangays' => Barangay::all()->select(['name', 'id']) ]);
     }
 
     public function submit(Request $request){
+        if (Auth::user()->residency) return redirect()->route('resident.dashboard');
         $validated = $request->validate([
             'barangay_id' => ['required']
         ]);
