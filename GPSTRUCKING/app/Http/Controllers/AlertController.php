466cf1d267d5dda08e35bd1238a537ee4ed33812
Emman-pip <parrenoemmanuel756@gmail.com>
2025-11-23 02:16:55 +0000

    Add alerts display for residents.

--- GPSTRUCKING/app/Http/Controllers/AlertController.php
+++ GPSTRUCKING/app/Http/Controllers/AlertController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class AlertController extends Controller
 {
     public function view() {
-        return Inertia::render('resident/alerts');
+        $alerts = Auth::user()->notifications()
+           ->where('type', 'App\Notifications\Alert')
+           ->get();
+        return Inertia::render('resident/alerts', ['alerts' => $alerts]);
     }
 }
--- GPSTRUCKING/resources/js/pages/resident/alerts.tsx
+++ GPSTRUCKING/resources/js/pages/resident/alerts.tsx
@@ -1,4 +1,5 @@
 import MapBarangay from '@/components/map/MapBarangay';
+import { cn } from '@/lib/utils';
 import MapView  from '@/components/map/MapView';
 import AppLayout from '@/layouts/resident/app-layout';
 import resident from '@/routes/resident';
@@ -6,22 +7,65 @@ import { type BreadcrumbItem } from '@/types';
 import { Head, usePage } from '@inertiajs/react';
 import { Barangay } from '../barangay/profileForm';
 import { UpdateBarangay } from './updateProfile';
+import { Bell, Clock, Dot, Siren } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Alerts',
-        href: resident.dashboard.url()
+        href: resident.alerts().url,
     }
 ];
 
-export default function Dashboard() {
+export interface Alert {
+    "id"?: number;
+    "type"?: string;
+    "notifiable_type"?: string
+    "notifiable_id"?: number;
+    "data"?: {
+        "title": string,
+        "message": string,
+        "tags": string[]
+    };
+    "read_at"?: string|null;
+    "created_at"?: string;
+    "updated_at"?: string;
+    "sender_id"?: number|null;
+}
+
+export default function Alerts({ alerts }: {
+    alerts: Alert[];
+}) {
     const { user } = usePage().props.auth;
+    console.log(alerts);
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
-            <div className="p-4">
-            hi world
-            </div>
+            <main className="p-2 pb-4">
+                <div>
+                    <div className="text-xl font-bold">Notifications & Feedback</div>
+                </div>
+                <div className="p-2 border border-current/30  flex flex-col gap-2 rounded-xl">
+                    <div className="font-semibold text-lg">Notification feed</div>
+                    <div className="flex flex-col gap-2 max-h-[100vh] overflow-y-auto">
+                        {
+                            alerts?.length > 0 ? alerts.map((alert, index) => {
+                                return <div className={ cn( "rounded-sm flex items-center gap-2  border border-current/50 p-2", alert.read_at ? "border-current/30" : "" )}>
+                                    <div className="bg-blue-500/40 p-2 rounded-lg">
+                                        <Bell className="text-blue-700"/>
+                                    </div>
+                                    <div>
+                                        <div className="font-semibold flex gap-1 items-center break-words">{ !alert.read_at ? <Dot size={30} className="text-red-500"/> : ""}{alert.data?.title}</div>
+                                        <div className="text-sm font-thin opacity-60 break-words">{alert.data?.message}</div>
+                                    </div>
+                                </div>
+                            }) : <div>
+                                No alerts yet!
+                            </div>
+
+                        }
+                    </div>
+                </div>
+            </main>
         </AppLayout>
     );
 }
