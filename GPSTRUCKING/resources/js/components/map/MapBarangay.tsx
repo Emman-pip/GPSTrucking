5640181422b5515c1485e360b4f9b5becf5d783c
Emman-pip <parrenoemmanuel756@gmail.com>
2025-11-24 23:45:54 +0000

    Create the harversine function for calculating distance.

--- GPSTRUCKING/resources/js/components/map/MapBarangay.tsx
+++ GPSTRUCKING/resources/js/components/map/MapBarangay.tsx
@@ -74,6 +74,25 @@ export interface PickUpSite {
     barangay?: string;
 }
 
+
+function getDistanceInMeters(coord1:[number, number], coord2:[number,number]) {
+    const R = 6371000; // Earth radius in meters
+    const toRad = x => x * Math.PI / 180;
+
+    const dLat = toRad(coord2[1] - coord1[1]);
+    const dLon = toRad(coord2[0] - coord1[0]);
+
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRad(coord1[1])) * Math.cos(toRad(coord2[1])) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c; // distance in meters
+}
+
+
 // TODO ROUTES!!!!
 // manually add routes by hand
 
