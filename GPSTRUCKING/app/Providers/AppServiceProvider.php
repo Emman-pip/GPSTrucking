83affd3a559ba3151e1ec5be852e010430d21742
Emman-pip <parrenoemmanuel756@gmail.com>
2025-12-03 04:56:37 +0000

    Make mariadb compatible.

--- GPSTRUCKING/app/Providers/AppServiceProvider.php
+++ GPSTRUCKING/app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Http\Middleware\EnsureLinkIsValid;
 use App\Http\Middleware\EnsureRole;
 use App\Http\Middleware\EnsureVerified;
 use App\Http\Middleware\isVerified;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        DB::statement("SET SESSION sql_mode = REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', '')");
         Route::aliasMiddleware('ensure_has_profile', EnsureVerified::class);
         Route::aliasMiddleware('ensure_verified', isVerified::class);
         Route::aliasMiddleware('ensure_role', EnsureRole::class);
