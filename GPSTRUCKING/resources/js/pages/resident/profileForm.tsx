75181c026f38a5347d3a80dbd5afb5eda933c067
Emman-pip <parrenoemmanuel756@gmail.com>
2025-12-08 19:43:20 +0800

    Make the resident comboboxes responsive.

--- GPSTRUCKING/resources/js/pages/resident/profileForm.tsx
+++ GPSTRUCKING/resources/js/pages/resident/profileForm.tsx
@@ -39,7 +39,7 @@ export default function ProfileForm({ barangays }: { barangays: Barangay[] }) {
         }
         e.preventDefault()
         post(resident.profileForm.submit().url,
-            {
+             {
             forceFormData: true,
         });
     }
@@ -49,42 +49,53 @@ export default function ProfileForm({ barangays }: { barangays: Barangay[] }) {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Barangay Personel Dashboard" />
 
-                <form onSubmit={submit} className="space-y-6 max-w-md mx-auto p-6 bg-white dark:bg-neutral-950 shadow-lg rounded-2xl w-full m-4">
+            <form onSubmit={submit} className="space-y-6 max-w-md mx-auto p-6 shadow-lg rounded-2xl w-full m-4">
+            <h1 className="text-lg font-bold">Set Residency</h1>
+            <div className="space-y-2">
+            <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger className="w-full flex justify-between items-center border px-3 py-2 rounded-lg dark:bg-neutral-800 dark:text-white">
+            {data.barangay || "Select Barangay"}
+            <ChevronsUpDown className="h-4 w-4 opacity-50" />
+            </PopoverTrigger>
 
-                    <div className="space-y-2">
-                        <Popover open={open} onOpenChange={setOpen}>
-                            <PopoverTrigger className="w-full flex justify-between items-center border px-3 py-2 rounded-lg dark:bg-neutral-800 dark:text-white">
-                                {data.barangay || "Select Barangay"}
-                                <ChevronsUpDown className="h-4 w-4 opacity-50" />
-                            </PopoverTrigger>
+            <PopoverContent
+        className="
+p-0
+w-full
+sm:w-64
+max-h-60
+overflow-y-auto
+dark:bg-neutral-800
+"
+        align="start"
+            >
+            <Command>
+            <CommandInput placeholder="Search barangay (Limited to Malvar Batangas)..." />
+            <CommandGroup>
+            {barangays.map((brgy) => (
+                <CommandItem
+                key={brgy.id}
+                onSelect={() => {
+                    setData("barangay", brgy.name);
+                    setData("barangay_id", brgy.id);
+                    setOpen(false);
+                }}
+                    >
+                    {brgy.name}
+                </CommandItem>
+            ))}
+        </CommandGroup>
+            </Command>
+            </PopoverContent>
+            </Popover>
 
-                            <PopoverContent className="p-0 w-[250px]">
-                                <Command>
-                                    <CommandInput placeholder="Search barangay (Limited to Malvar batangas)..." />
-                                    <CommandGroup>
-                                        {barangays.map((brgy) => (
-                                            <CommandItem
-                                                key={brgy.id}
-                                                onSelect={() => {
-                                                    setData("barangay", brgy.name);
-                                                    setData("barangay_id", brgy.id);
-                                                    setOpen(false);
-                                                }}
-                                            >
-                                                {brgy.name}
-                                            </CommandItem>
-                                        ))}
-                                    </CommandGroup>
-                                </Command>
-                            </PopoverContent>
-                        </Popover>
             {errors.barangay_id && <div className="text-red-500">{errors.barangay_id}</div>}
-                    </div>
+        </div>
 
-                    <Button className="w-full">
+            <Button className="w-full">
             Submit
-                    </Button>
-                </form>
-        </AppLayout>
+        </Button>
+            </form>
+            </AppLayout>
     );
 }
--- GPSTRUCKING/resources/js/pages/resident/updateProfile.tsx
+++ GPSTRUCKING/resources/js/pages/resident/updateProfile.tsx
@@ -1,12 +1,12 @@
 import { Button } from '@/components/ui/button';
 import { useState } from "react";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
 } from "@/components/ui/dialog"
 import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
 import { Command, CommandGroup, CommandItem, CommandInput } from "@/components/ui/command";
@@ -45,50 +45,61 @@ export function UpdateBarangay({ barangays, barangayData }: { barangays: Baranga
         <Dialog>
             <DialogTrigger>
             <Button variant={"outline"}><MapPin/>Change Residency</Button>
-        </DialogTrigger>
+            </DialogTrigger>
             <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Update residency</DialogTitle>
-                    <DialogDescription>
-                        <form onSubmit={submit} className="space-y-6 max-w-md mx-auto p-6 bg-white dark:bg-neutral-950 rounded-2xl w-full m-4">
+            <DialogHeader>
+            <DialogTitle>Update residency</DialogTitle>
+            <DialogDescription>
+            <form onSubmit={submit} className="space-y-6 max-w-md mx-auto p-6 bg-white dark:bg-neutral-950 rounded-2xl w-full m-4">
 
-                            <div className="space-y-2">
-                                <Popover open={open} onOpenChange={setOpen}>
-                                    <PopoverTrigger className="w-full flex justify-between items-center border px-3 py-2 rounded-lg dark:bg-neutral-800 dark:text-white">
-                                        {data.barangay || "Select Barangay"}
-                                        <ChevronsUpDown className="h-4 w-4 opacity-50" />
-                                    </PopoverTrigger>
+            <div className="space-y-2">
+            <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger className="w-full flex justify-between items-center border px-3 py-2 rounded-lg dark:bg-neutral-800 dark:text-white">
+            {data.barangay || "Select Barangay"}
+            <ChevronsUpDown className="h-4 w-4 opacity-50" />
+            </PopoverTrigger>
 
-                                    <PopoverContent className="p-0 w-[250px]">
-                                        <Command>
-                                            <CommandInput placeholder="Search barangay (Limited to Malvar batangas)..." />
-                                            <CommandGroup>
-                                                {barangays.map((brgy) => (
-                                                    <CommandItem
-                                                        key={brgy.id}
-                                                        onSelect={() => {
-                                                            setData("barangay", brgy.name);
-                                                            setData("barangay_id", brgy.id);
-                                                            setOpen(false);
-                                                        }}
-                                                    >
-                                                        {brgy.name}
-                                                    </CommandItem>
-                                                ))}
-                                            </CommandGroup>
-                                        </Command>
-                                    </PopoverContent>
-                                </Popover>
-                                {errors.barangay_id && <div className="text-red-500">{errors.barangay_id}</div>}
-                            </div>
+            <PopoverContent
+        className="
+p-0
+w-full
+sm:w-64
+max-h-60
+overflow-y-auto
+dark:bg-neutral-800
+"
+        align="start"
+            >
+            <Command>
+            <CommandInput placeholder="Search barangay (Limited to Malvar Batangas)..." />
+            <CommandGroup>
+            {barangays.map((brgy) => (
+                <CommandItem
+                key={brgy.id}
+                onSelect={() => {
+                    setData("barangay", brgy.name);
+                    setData("barangay_id", brgy.id);
+                    setOpen(false);
+                }}
+                    >
+                    {brgy.name}
+                </CommandItem>
+            ))}
+        </CommandGroup>
+            </Command>
+            </PopoverContent>
+            </Popover>
 
-                            <Button className="w-full">
+            {errors.barangay_id && <div className="text-red-500">{errors.barangay_id}</div>}
+        </div>
+
+            <Button className="w-full">
             Save
-                            </Button>
-                        </form>
-                    </DialogDescription>
-                </DialogHeader>
+        </Button>
+            </form>
+            </DialogDescription>
+            </DialogHeader>
             </DialogContent>
-        </Dialog>
+            </Dialog>
     );
 }
